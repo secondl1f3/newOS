f94b29051e49f80bffedc6e0b25f1e727e0fbfa3
secondl1f3 <ahmad.fajar@outlook.co.id>
2020-10-08 09:42:43 -0700

    Video 1 write your own operating system

---
+++ kernel.cpp
@@ -0,0 +1,12 @@
+void printf(char* str)
+{
+    unsigned short* VideoMemory = (unsigned short*)0xb8000;
+    for(int i = 0; str[i] != '\0'; ++i)
+        VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
+}
+
+extern "C" void kernelMain(void* multiboot_structure, unsigned int magicnumber)
+{
+    printf("Hello World! ---- http://www.kau.edu.sa");
+    while (1);
+}
